0b1b47f5ac78cec0a395059edf375aeaa9290f46
JUAN DE DIOS VALVERDE <jvh396@inlumine.ual.es>
2023-04-24 19:00:51 +0200

    Search filter

--- src/main/java/com/example/demo/HeroRepository.java
+++ src/main/java/com/example/demo/HeroRepository.java
@@ -1,9 +1,12 @@
 
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface HeroRepository extends CrudRepository<Hero, Long>{   
+   List<Hero> findByNameContainingIgnoreCase(String name);
 }
